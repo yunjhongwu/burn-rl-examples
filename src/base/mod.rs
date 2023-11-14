ab38ff50c9220681fec6429a04df99dfee31fab4
Yun-Jhong Wu <yjwuam@gmail.com>
2023-11-14 10:52:08 -0600

    Model trait and cleanups

--- src/base/mod.rs
+++ src/base/mod.rs
@@ -1,9 +1,11 @@
 mod action;
 mod memory;
+mod model;
 mod snapshot;
 mod state;
 
 pub use action::Action;
 pub use memory::{Memory, Transition};
+pub use model::Model;
 pub use snapshot::Snapshot;
 pub use state::State;
--- src/components/agent.rs
+++ src/components/agent.rs
@@ -1,6 +1,6 @@
 use crate::base::{Action, State};
 
-pub trait Agent: Default {
+pub trait Agent {
     type StateType: State;
     type ActionType: Action;
 
